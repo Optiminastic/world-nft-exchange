a7b98184f08ab0aa107c867665e81b2879d61a39
adnan007d <adnanmansuri54@gmail.com>
2022-12-08 17:38:58 +0530

    create token

--- contracts/IndianNFTExchange.sol
+++ contracts/IndianNFTExchange.sol
@@ -12,11 +12,33 @@ contract IndianNFTExchange is ERC721URIStorage, Ownable, PullPayment {
 
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
+    Counters.Counter private _itemsListed;
 
-    // address public owner;
     receive() external payable {}
 
     uint256 listingPrice = 0.025 ether;
+
+    struct INEItem {
+        uint256 id;
+        string tokenURI;
+        uint256 price;
+        address payable seller;
+        address payable owner;
+        address payable originalOwner;
+        bool currentlyListed;
+    }
+
+    event INEItemCreated(
+        uint256 id,
+        string tokenURI,
+        uint256 price,
+        address payable seller,
+        address payable owner,
+        address payable originalOwner,
+        bool currentlyListed
+    );
+
+    mapping(uint256 => INEItem) private INEItems;
 
     constructor() ERC721("IndianNFTExchange", "INE") {}
 
@@ -32,6 +54,46 @@ contract IndianNFTExchange is ERC721URIStorage, Ownable, PullPayment {
         address payable payee
     ) public virtual override onlyOwner {
         super.withdrawPayments(payee);
-        payee.transfer(address(this).balance);
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function createINEItem(
+        string memory tokenURI,
+        uint256 price
+    ) public payable returns (uint256) {
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+        require(price > 0, "Price must be greater than 0");
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        _itemsListed.increment();
+
+        INEItems[newItemId] = INEItem(
+            newItemId,
+            tokenURI,
+            price,
+            payable(msg.sender),
+            payable(address(0)),
+            payable(msg.sender),
+            true
+        );
+
+        _transfer(msg.sender, address(this), newItemId);
+
+        emit INEItemCreated(
+            newItemId,
+            tokenURI,
+            price,
+            payable(msg.sender),
+            payable(address(0)),
+            payable(msg.sender),
+            true
+        );
+
+        return newItemId;
     }
 }
